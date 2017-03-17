1fde03b594fc9b98333396320c8e7f406ec6e21c
Uday Ayyagari <ua337@nyu.edu>
2017-03-17 02:48:55 -0400

    deleting unused vars

--- Branch.cpp
+++ Branch.cpp
@@ -1,4 +1,3 @@
-// Referenced from the cache simulator lab 2
 
 #include <iostream>
 #include <fstream>
@@ -22,26 +21,20 @@ int main(int argc, char* argv[])
     config branchconfig;
     ifstream branch_params;
     branch_params.open(argv[1]);
+    
     while(!branch_params.eof())  // read config file
     {
         branch_params>>branchconfig.size1;
     }
     
-   // cout<<"\n"<<branchconfig.size1<<" \n";
-  
     long x = pow(2,branchconfig.size1);
-    //cout<<x<<"\n";
     int A[x-1];
     
     for(int i =0;i<x;i++)
     {
         A[i] = 3;
     }
-    /*
-    for(int i =0;i<x;i++)
-    {
-        cout<<A[i][0]<<"\n";
-    }*/
+    
     int misprediction =0;
     int total =0;
     int correct =0;
@@ -60,120 +53,74 @@ int main(int argc, char* argv[])
     unsigned int integeraddress;
     bitset<32> bitaddress;
     bitset<32> index;
-    //int arr_index;
+
     if (traces.is_open()&&tracesout.is_open()){
         while (getline (traces,line)){   // read mem access file and access Cache
             
             istringstream iss(line);
-            if (!(iss >> hexa >> bit)) {break;}
-            stringstream saddr(hexa);
             
-            //cout<<bit<<" ";
-            
+            if (!(iss >> hexa >> bit)){
+                break;
+            }
+            stringstream saddr(hexa);            
             saddr >> std::hex >> integeraddress;
             total ++;
-            
-            
             bitaddress = bitset <32> (integeraddress);
-            //cout<<bitaddress<<" ";
             index = (bitset<32>((bitaddress.to_string().substr(m,31))).to_ulong());
-           // cout<<index<<" ";
             int arr_index = index.to_ulong();
-            //cout<<arr_index<<" ";
             
-             if (bit == 0)
-             {    
-                  //cout<<"Not Taken \t";
-                
-                 if(A[arr_index]==0)
-                 {
+            if (bit == 0){    
+             
+                 if(A[arr_index]==0){
                      A[arr_index]=0;
                      correct++;
-                     //cout<< "CORRECT";
                      j = 0;
-
                  }
-                 else if(A[arr_index]==1)
-                        {
-                            A[arr_index]=0;
-                       //     cout<< "CORRECT";
-                            correct++;
-                            j = 0;
-
-                        }
-                        else if(A[arr_index]==2)
-                            {
-                                A[arr_index]=0;
-                                misprediction++;
-                                j = 1;
-                     //           cout<< "MISPREDICTION";
-                            }
-                else if (A[arr_index]==3)
-                        {
-                            A[arr_index]=2;
-                            misprediction++;
-                            j = 1;
-                   //         cout<< "MISPREDICTION";
-                        }
-                 
-                 
-                 
-                 
+                 else if(A[arr_index]==1){
+                     A[arr_index]=0;
+                     correct++;
+                     j = 0;
+                 }
+                 else if(A[arr_index]==2){
+                     A[arr_index]=0;
+                     misprediction++;
+                     j = 1;
+                 }
+                 else if (A[arr_index]==3)
+                     A[arr_index]=2;
+                     misprediction++;
+                     j = 1;
+                 }
             }
-             else 
-             {    
-                   //Implement by you:
-                 //cout<<"Taken \t";
-                 if(A[arr_index]==3)
-                    {
+            else{    
+                 if(A[arr_index]==3){
                         A[arr_index]=3;
                         correct++;
-                      //  cout<< "Correct ";
                         j = 1;
-                        
-
-                    }
-                 else if(A[arr_index]==2)
-                        {
-                            A[arr_index]=3;
-                            correct++;
-                            //cout<< "Correct ";
-                            j = 1;
+                 }
+                 else if(A[arr_index]==2){
+                        A[arr_index]=3;
+                        correct++;
+                        j = 1;
                             
-                        }
-                    else if(A[arr_index]==1)
-                            {
-                                A[arr_index]=3;
-                                misprediction++;
-                                j = 0;
-                               // cout<< "MISPREDICTION";
-                                
-                            }
-                        else if (A[arr_index]==0)
-                            {
+                 }
+                 else if(A[arr_index]==1){
+                        A[arr_index]=3;
+                        misprediction++;
+                        j = 0;
+                 }
+                 else if (A[arr_index]==0){
                                 A[arr_index]=1;
                                 misprediction++;
-                               // cout<< "MISPREDICTION";
                                j = 0;
-                                
-                            }
-                 
-                 
-                 
-                 
-                 
-                  }
-                                // tracesout<< x<< endl;
-          //  cout<<" CA"<<A[arr_index] <<" \n";
-                                //cout<<hexa<<" \n";
-            tracesout<<j<<endl;
-          // output here if anything
+                 }
+           }
+           tracesout<<j<<endl;
              
         }
         cout<<"Mispredicted is " <<misprediction <<"\t";
         cout<<"correct is "<< correct <<"\n";
         cout<<"total" << total <<"\n";
-   //     cout<<"works nah  \n";
         float xx = (float(misprediction) / float(total)) * 100 ;
         cout<<"misprediction rate " << xx <<" \n";
         traces.close();
@@ -181,11 +128,5 @@ int main(int argc, char* argv[])
     }
     else cout<< "Unable to open trace or traceout file ";
 
-
-   
-    
-  
-
-   
     return 0;
 }
